e676b3eb70c7acfe9deed589bccdfbed0285bab4
Busidoway <busidowayfarer@gmail.com>
2021-10-19 20:13:05 +0500

    v 0.004

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('meta-title')Главная@endsection
+
+@section('content')
+    <div>Megatest</div>
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,20 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-//  mix.autoload({
-//     'jquery': ['$', 'window.jQuery', "jQuery", "window.$", "jquery", "window.jquery"],
-//     'popper.js/dist/umd/popper.js': ['Popper', 'window.Popper']
-// });
-
 mix.js([
         'resources/js/app.js',
         'node_modules/popper.js/dist/umd/popper.js',
         'node_modules/bootstrap/dist/js/bootstrap.js',
     ], 'public/js')
     .vue()
-    // .sass([
-    //     'resources/sass/app.scss',
-    //     'node_modules/bootstrap/scss/bootstrap.scss'
-    // ], 'public/css')
     .sass('resources/sass/app.scss', 'public/css')
     .css('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css');
